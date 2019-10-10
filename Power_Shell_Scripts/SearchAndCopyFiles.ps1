c98b18c835829c80abad949a5126501d5942d4b8
mvail52 <matthewvail@hotmail.com>
2019-10-09 22:03:56 -0400

    Create SearchAndCopyFiles.ps1
    Created this so find files with a similar name and copy them all to one folder.

---
+++ Power_Shell_Scripts/SearchAndCopyFiles.ps1
@@ -0,0 +1,19 @@
+﻿$location = Read-Host 'What is the location you would like to search?'
+$destination =  Read-Host 'What is destination you would files copied to?'
+$search = Read-Host 'Enter the of the item'
+
+Set-Location  $location.ToString()
+
+foreach( $e in Get-ChildItem -Path $location -Name -Recurse)
+    {
+
+    if ($e.ToString() -like "*$search*")
+        {
+            $name = Split-Path(Split-Path "$e") -leaf
+            $dest = $destination+ "\" + $name.ToString()
+
+            Copy-Item "$e" -Destination $dest
+            #if you want to see the files it is copying then uncomment the next line
+            #Write-Output "Copied " + $e.ToString()
+        }
+    }
