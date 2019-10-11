99a517161f1839827a44fc807161ff24da304e3f
mvail52 <matthewvail@hotmail.com>
2019-10-11 12:49:50 -0400

    Create Shrink_All_Logs.sql
    Shrink All database Logs

---
+++ SQL_Scripts/Shrink_All_Logs.sql
@@ -0,0 +1,38 @@
+DECLARE @site VARCHAR(MAX)
+DECLARE @log VARCHAR(MAX)
+DECLARE @sql VARCHAR(MAX)
+
+
+DECLARE [cur] CURSOR FAST_FORWARD READ_ONLY FOR
+  SELECT DISTINCT [db].[name] AS [DBName]
+    FROM [sys].[master_files] [mf]
+    INNER JOIN [sys].[databases] [db]
+      ON [db].[database_id] = [mf].[database_id]
+   WHERE [type_desc] = 'LOG'
+   ORDER BY [db].[name]
+
+OPEN [cur]
+
+FETCH NEXT FROM [cur]
+ INTO @site
+
+WHILE @@FETCH_STATUS = 0
+  BEGIN
+    SET @log =
+      (SELECT DISTINCT [mf].[name]
+         FROM [sys].[master_files] [mf]
+         INNER JOIN [sys].[databases] [db]
+           ON [db].[database_id] = [mf].[database_id]
+        WHERE [type_desc] = 'LOG'
+          AND [db].[name] = @site)
+
+    SET @sql = 'use ' + @site + '; DBCC SHRINKFILE (N''' + @log + ''', 0, TRUNCATEONLY)'
+    EXEC (@sql)
+    PRINT @sql
+    FETCH NEXT FROM [cur]
+     INTO @site
+
+  END
+DEALLOCATE [cur]
+
+
